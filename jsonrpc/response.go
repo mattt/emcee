f5cf6c59bffeba71f30d9d716122b65cdf297ccc
Mattt Zmuda <mattt@loopwork.com>
2025-01-22 04:04:10 -0800

    Define jsonrpc.Result type

--- jsonrpc/response.go
+++ jsonrpc/response.go
@@ -1,15 +1,18 @@
 package jsonrpc
 
+// Result represents a map of string keys to arbitrary values
+type Result interface{}
+
 // Response represents a JSON-RPC response object
 type Response struct {
-	Version string      `json:"jsonrpc"`
-	Result  interface{} `json:"result,omitempty"`
-	Error   *Error      `json:"error,omitempty"`
-	ID      ID          `json:"id"`
+	Version string `json:"jsonrpc"`
+	Result  Result `json:"result,omitempty"`
+	Error   *Error `json:"error,omitempty"`
+	ID      ID     `json:"id"`
 }
 
 // NewResponse creates a new Response object
-func NewResponse(id interface{}, result interface{}, err *Error) Response {
+func NewResponse(id interface{}, result Result, err *Error) Response {
 	respID, _ := NewID(id)
 
 	return Response{
